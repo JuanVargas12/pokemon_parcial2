e516eef92482ca183d4dc5dfa2e5befd2fac4b68
Juan Vargas <jd.vargasl1@uniandes.edu.co>
2024-11-20 08:44:07 -0500

    Detalle y listar

---
+++ src/app/pokemon/pokemon-detalle/pokemon-detalle.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-detalle',
+  templateUrl: './pokemon-detalle.component.html',
+  styleUrls: ['./pokemon-detalle.component.css']
+})
+export class PokemonDetalleComponent implements OnInit {
+  pokemon: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.pokemonService.getPokemon(id!).subscribe((data) => {
+      this.pokemon = data;
+    });
+  }
+}
---
+++ src/app/pokemon/pokemon-listar/pokemon-listar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-listar',
+  templateUrl: './pokemon-listar.component.html',
+  styleUrls: ['./pokemon-listar.component.css']
+})
+export class PokemonListarComponent implements OnInit {
+  pokemon: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private PokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.PokemonService.getPokemon(id!).subscribe((data) => {
+      this.pokemon = data;
+    });
+  }
+}
+
