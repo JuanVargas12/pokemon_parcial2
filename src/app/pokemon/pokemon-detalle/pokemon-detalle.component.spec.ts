0f3ad4c9b28faf7c363c4551e395ba9086f7e035
Juan Vargas <jd.vargasl1@uniandes.edu.co>
2024-11-20 08:50:48 -0500

    coreccion detalle y listar

--- src/app/pokemon/pokemon-detalle/pokemon-detalle.component.spec.ts
+++ src/app/pokemon/pokemon-detalle/pokemon-detalle.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('PokemonDetalleComponent', () => {
   let component: PokemonDetalleComponent;
   let fixture: ComponentFixture<PokemonDetalleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach((() => {
     TestBed.configureTestingModule({
       declarations: [ PokemonDetalleComponent ]
     })
